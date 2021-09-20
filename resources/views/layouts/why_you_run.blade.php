d9621bc3cd0976ac55a5a83c9040d6d3500f9f03
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2021-09-20 17:37:41 +0900

    タイトル

--- resources/views/layouts/why_you_run.blade.php
+++ resources/views/layouts/why_you_run.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'new_balance') }}</title>
+    <title>new_balance why you run キャンペーン</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
